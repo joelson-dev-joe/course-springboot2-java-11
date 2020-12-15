a6a863973d8dc2ace30752d82c27e5768c232f9c
joelson-dev-joe <joelson.dh@gmail.com>
2020-12-15 16:57:56 -0300

    Product

--- src/main/java/com/joemarvi/course/CourseApplication.java
+++ src/main/java/com/joemarvi/course/CourseApplication.java
@@ -3,6 +3,8 @@ package com.joemarvi.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Inicio da aplicação 
+
 @SpringBootApplication
 public class CourseApplication {
 
